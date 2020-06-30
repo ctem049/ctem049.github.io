70ccf7e059fdf7677483deebb1a424eb9f8e4bf8
cteem049 <cteem049@gmail.com>
2020-06-30 23:26:43 +0800

    2020.06.30

---
+++ js/data.js
@@ -0,0 +1,38 @@
+var data = {
+    updateTime: '2020-06-30',
+    content: [{
+            title: '最近在看',
+            type: 'video',
+            list: [
+                { content: `[直播] <a href="https://www.huya.com/lastpriest" target="_blank">老中医 @虎牙</a> 炉石传说`, updateTime: '2020-06-30' },
+                { content: `[直播] <a href="https://live.bilibili.com/528" target="_blank">痒局长 @Bilibili</a> 绝地求生 狼人杀`, updateTime: '2020-06-30' },
+                { content: `[直播] <a href="https://live.bilibili.com/462" target="_blank">老骚豆腐 @Bilibili</a> 第五人格 狼人杀`, updateTime: '2020-06-28' },
+                { content: `[直播] <a href="https://live.bilibili.com/6" target="_blank">LPL @Bilibili</a> 英雄联盟`, updateTime: '2020-06-30' },
+            ]
+        },
+        {
+            title: '最近在听',
+            type: 'music',
+            list: [
+                { content: `<a href="https://y.qq.com/n/yqq/song/001jyikY2o3FrL.html" target="_blank">椎名豪, 中川奈美 - 竈門炭治郎のうた</a>`, updateTime: '2020-06-28' }
+            ]
+        },
+        {
+            title: '最近在玩',
+            type: 'game',
+            list: [
+                { content: `<a href="https://lol.qq.com" target="_blank">英雄联盟</a> League of Legends`, updateTime: '2020-06-30' },
+                { content: `<a href="https://mc.163.com" target="_blank">我的世界</a> Minecraft`, updateTime: '2020-06-24' },
+                { content: `<a href="https://store.steampowered.com/app/493340/Planet_Coaster/" target="_blank">过山车之星</a> Planet Coaster`, updateTime: '2020-06-08' },
+                { content: `<a href="https://sc2.blizzard.cn/" target="_blank">星际争霸2</a> Starcraft II`, updateTime: '2019-12-25' },
+            ]
+        },
+        {
+            title: '最近在读',
+            type: 'game',
+            list: [
+                { content: `<a href="https://mp.weixin.qq.com/s/mI_zTi8xFIYoZVlY6oQJtw" target="_blank">家庭的未来：从社会原子化到社会化抚养</a>`, updateTime: '2020-06-30' }
+            ]
+        }
+    ]
+}
---
+++ js/main.js
@@ -0,0 +1,35 @@
+var app = new Vue({
+    el: '#app',
+    data: { data: data },
+    methods: {
+        daf(value, fmt = 'yyyy.MM.dd') {
+            return new Date(value).format(fmt);
+        },
+        /**
+         * 计算日期颜色饱和度
+         * @returns {number} 0%~100%
+         */
+        cacColor(date) {
+            var now = new Date(data.updateTime).getTime();
+            var d = new Date(date).getTime();
+            var time = Math.log(now - d);
+            // 18 ~ 24
+            if (time <= 18) {
+                return '100%'
+            } else if (time >= 24) {
+                return '0%'
+            } else {
+                var t = 100 - (time - 18) / 6 * 100;
+                return Math.floor(t).toString() + '%';
+            }
+        },
+        cacStyle(date) {
+            return `hsl(200,${this.cacColor(date)},50%)`;
+        }
+    },
+    filters: {
+        daf(value, fmt = 'yyyy.MM.dd') {
+            return new Date(value).format(fmt);
+        }
+    }
+})
