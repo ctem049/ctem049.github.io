638d31272a589858d44093c03f6a1be8b8a82693
yiiiru_ <66953472+ctem049@users.noreply.github.com>
2020-12-15 10:10:11 +0800

    update playlist

--- player/playlist.js
+++ player/playlist.js
@@ -1,12 +1,91 @@
-var playlists = [
-    {
-        name: '2010b', content: [
-            "银临、云の泣 - 锦鲤抄", "李荣浩 - 不将就", "许美静 - 城里的月光", "周深 - 大鱼", "司南 - 冬眠", "欧得洋 - 孤单北半球", "王菲 - 红豆", "霍尊 - 卷珠帘", "梁静茹 - 暖暖", "王力宏 - 你不知道的事", "孙燕姿 - 逆光", "徐秉龙、桃十五 - 青柠", "林俊杰 - 她說", "许嵩、刘美麟 - 温泉", "周杰伦、温岚 - 屋顶", "杨沛宜 - 左手右手"
-        ]
+var playlistmd = `# 2010b
+
+|artist|name|filename|lyric|cover|
+|-----|-----|-----|-----|-----|
+|银临,云の泣|锦鲤抄|银临、云の泣 - 锦鲤抄.flac||https://y.gtimg.cn/music/photo_new/T002R300x300M000001ojbuJ1JoQA1_1.jpg|
+|李荣浩|不将就|李荣浩 - 不将就.flac|||
+|许美静|城里的月光|许美静 - 城里的月光.flac|||
+|周深|大鱼|周深 - 大鱼.flac|||
+|司南|冬眠|司南 - 冬眠.flac|||
+|欧得洋|孤单北半球|欧得洋 - 孤单北半球.flac|||
+|王菲|红豆|王菲 - 红豆.flac|||
+|霍尊|卷珠帘|霍尊 - 卷珠帘.flac|||
+|梁静茹|暖暖|梁静茹 - 暖暖.flac|||
+|王力宏|你不知道的事|王力宏 - 你不知道的事.flac|||
+|孙燕姿|逆光|孙燕姿 - 逆光.flac|||
+|桃十五&徐秉龙|青柠|徐秉龙、桃十五 - 青柠.flac|||
+|林俊杰|她说|林俊杰 - 她說.flac|||
+|许嵩/刘美麟|温泉|许嵩、刘美麟 - 温泉.flac|||
+|周杰伦&温岚|屋顶|周杰伦、温岚 - 屋顶.flac|||
+|杨沛宜|左手右手|杨沛宜 - 左手右手.flac|||`
+
+// 获取playlist.md
+function getPlaylist() {
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", "./playlist.md?"+new Date().getTime(), false); // 同步请求
+    oReq.send(null);//发送数据需要自定义，这里发送的是JSON结构
+    var result = oReq.responseText;//响应结果
+    return result;
+}
+try {
+    var result = getPlaylist();
+    if (result) {
+        playlistmd = result;
     }
-    ,
-    {
-        name: '许嵩新', content: ["许嵩 - 雨幕", "许嵩、刘美麟 - 温泉", "许嵩 - 通关", "许嵩 - 全世界最好的你", "许嵩 - 蝴蝶的时间", "许嵩 - 我乐意", "许嵩 - 明智之举"]
-    },
-    { name:'2012-4', content: ["SNH48 - 夜蝶", "CKG48 - 人鱼", "鞠婧祎 - 缘尽世间", "GNZ48 - 就差一点点", "鞠婧祎 - 叹云兮"]}
-]
+} catch (e) {
+    console.log(e);
+}
+
+var mdsplit = playlistmd.split('\n')
+var playlists = [{
+    name: 'plname', content: [
+        { artist: '', name: '', filename: '', lyric: '', cover: '' }
+    ]
+}]
+
+// md分割为播放列表
+playlists = mdcomp(mdsplit)
+
+function mdcomp(mdsplit) {
+    var inlist = false
+    var intable = false
+    var tablestart = true
+    var keys = []
+    var values = []
+    var list = {}
+    var lists = []
+    mdsplit.forEach(v => {
+        if (v.startsWith('# ')) {
+            if (inlist) {
+                lists.push(list)
+            }
+            inlist = true
+            list = { name: v.slice(2), content: [] }
+        }
+        if (v.startsWith('|')) {
+            intable = true
+            if (tablestart) {
+                values = v.split('|')
+                values.shift(1)
+                var content = {}
+                keys.forEach((v,i)=>{
+                    content[v] = values[i]
+                })
+                // console.log(keys,values,content)
+                list.content.push(content)
+            } else {
+                if (v.startsWith('|-')) {
+                    tablestart = true
+                } else {
+                    keys = v.split('|').filter(Boolean)
+                    // console.log('keys', keys, v)
+                }
+            }
+        } else {
+            tablestart = false
+            intable = false
+        }
+    })
+    lists.push(list)
+    return lists
+}
