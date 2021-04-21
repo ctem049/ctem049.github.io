495411806fa65b9983d94fb8446fe2bd08be635a
OnO <66953472+ctem049@users.noreply.github.com>
2021-04-21 11:30:05 +0800

    update ono.js

---
+++ js/ono.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d11ee94665b34fd48cfdbf5e8424fa25";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
