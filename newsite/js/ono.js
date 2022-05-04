75bba4cd1d2d68c1a0ff5b8f9eb8e9bafa25c580
ctem049 <66953472+ctem049@users.noreply.github.com>
2022-05-04 09:48:39 +0800

    update 220504_094832

--- newsite/js/ono.js
+++ newsite/js/ono.js
@@ -1,6 +1,9 @@
 var app = new Vue({
     el: '#y-container',
-    data: { logor: true },
+    data: {
+        logor: false,
+        
+    },
     methods: {
         daf(value, fmt = 'yyyy.MM.dd') {
             return new Date(value).format(fmt);
